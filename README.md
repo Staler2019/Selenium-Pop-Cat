8fde19e74f706cbaa02dc6def57d0bd5abb39363
Staler2019 <abcde121234@gmail.com>
2023-09-04 10:13:46 +0800

    move out from My-Side-Proj

---
+++ README.md
@@ -0,0 +1,5 @@
+# popcat selenium
+
+## 有人寫的研究書
+
+[HackMD - Popcat](https://hackmd.io/HU0nqTPwQAe3USd0705Lsw?view&fbclid=IwAR1KVLqFEAhpPjykZQzkNZp6VtLhjgQmH5hq2PGWskmXqB23mPESnGLoMe8)
---
+++ catClicker.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Edge("msedgedriver.exe")
+driver.get(
+    "https://popcat.click/?fbclid=IwAR0XlDcofkycLI1bMB66ekXM_yLnTnX8RSC-X1AES--iK5rKjPIt_NIrVkg"
+)
+
+action = ActionChains(driver)
+action.click(driver.find_element_by_xpath('//*[@id="app"]/div'))
+
+while 1:
+    action.perform()
+
+driver.quit()
